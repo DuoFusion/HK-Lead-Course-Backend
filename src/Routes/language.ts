4ad4c013ff326511e1be90ffcd00df845eca74fd
Palakduofusion <gohelpalak14@gmail.com>
2025-08-20 18:07:33 +0530

    commit

--- src/Routes/language.ts
+++ src/Routes/language.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import { adminJWT } from '../helper'
-import { addLanguage, editLanguage } from '../controllers/language'
+import { addLanguage, deleteLanguage, editLanguage, getLanguage } from '../controllers/language'
 
 const router = express.Router()
 
 router.use(adminJWT)
 router.post('/add',addLanguage)
 router.post('/edit',editLanguage)
+router.get('/',getLanguage)
+router.delete('/delete/:id',deleteLanguage)
 
 export const languageRoutes = router
--- src/controllers/language/index.ts
+++ src/controllers/language/index.ts
@@ -1,42 +1,97 @@
 import { apiResponse } from "../../common";
 import { languageModel } from "../../database/models/language";
 import { reqInfo, responseMessage } from "../../helper"
-import { createData, getFirstMatch, updateData } from "../../helper/database_service";
+import { countData, createData, deleteData, getData, getFirstMatch, updateData } from "../../helper/database_service";
 
 
 
 const ObjectId = require('mongoose').Types.ObjectId
 
 
-export const addLanguage = async(req,res)=>{
+export const addLanguage = async (req, res) => {
     reqInfo(req)
-    try{
+    try {
         const body = req.body;
-        let isExist = await getFirstMatch(languageModel,{priority:body.priority},{},{lean:true});
-        if(isExist) return res.status(404).json(new apiResponse(404,responseMessage.dataAlreadyExist('prority'),{},{}));
+        let isExist = await getFirstMatch(languageModel, { priority: body.priority }, {}, { lean: true });
+        if (isExist) return res.status(404).json(new apiResponse(404, responseMessage.dataAlreadyExist('prority'), {}, {}));
 
-        const response = await createData(languageModel,body);
-        return res.status(200).json(new apiResponse(200,responseMessage.addDataSuccess('Language'),response,{}))
+        const response = await createData(languageModel, body);
 
-    }catch(error){
+        return res.status(200).json(new apiResponse(200, responseMessage.addDataSuccess('Language'), response, {}))
+
+    } catch (error) {
         console.log(error);
-        return res.status(500).json(new apiResponse(500,responseMessage.internalServerError,{},error));
-        
+        return res.status(500).json(new apiResponse(500, responseMessage.internalServerError, {}, error));
+
     }
 }
 
 
-export const editLanguage = async(req,res)=>{
+export const editLanguage = async (req, res) => {
+    reqInfo(req)
+    try {
+        const body = req.body;
+        console.log("body", body);
+
+        let isExist = await getFirstMatch(languageModel, { priority: body.priority, _id: { $ne: new ObjectId(body.languageId) } }, {}, { lean: true });
+        if (!isExist) return res.status(404).json(new apiResponse(404, responseMessage.dataAlreadyExist('prority'), {}, {}));
+
+        const response = await updateData(languageModel, { _id: new ObjectId(body.languageId), isDeleted: false }, body, { new: true });
+        if (!response) return res.status(404).json(new apiResponse(404, responseMessage.getDataNotFound('Category'), {}, {}));
+
+        return res.status(200).json(new apiResponse(200, responseMessage.updateDataSuccess('Language'), response, {}));
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(new apiResponse(500, responseMessage.internalServerError, {}, error));
+
+    }
+}
+
+export const getLanguage = async(req,res)=>{
     reqInfo(req)
     try{
-        const body = req.body;
-        console.log("body",body);
-        
-        let isExist = await getFirstMatch(languageModel,{priority:body.priority,_id:{$ne:new ObjectId(body.languageId)}},{},{lean:true});
-        if(!isExist) return res.status(404).json(new apiResponse(404,responseMessage.dataAlreadyExist('prority'),{},{}));
+       let {search , page , limit} =req.query,options:any = {lean:true},criteria:any = {isDeleted:false};
+       if(search) {
+        criteria.title = { $regex: search, $options: 'si'};
+       } 
 
-        const response = await updateData(languageModel,{_id:new ObjectId(body.languageId)},body,{});
-        return res.status(200).json(new apiResponse(200,responseMessage.updateDataSuccess('Language'),response,{}));
+       options.sort = { priority: 1, createdAt: -1 };
+
+       if(page && limit){
+        options.skip = (parseInt(page)-1)*parseInt(limit);
+        options.limit = parseInt(limit);
+       }
+
+       const responce = await getData(languageModel,criteria,{},options);
+       const totalCount = await countData(languageModel,criteria);
+
+       const stateObj = {
+           page : parseInt(page)||1,
+           limit: parseInt(limit)||totalCount,
+           page_limit: Math.ceil(totalCount/(parseInt(limit)|| totalCount))||1,
+
+       };
+       return res.status(200).json(new apiResponse(200,responseMessage?.getDataSuccess('Language'),{language_data:responce,totalData:totalCount,state:stateObj},{}));
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json(new apiResponse(500,responseMessage.internalServerError,{},error)) 
+    }
+}
+
+export const deleteLanguage = async(req,res)=>{
+    reqInfo(req)
+    try{
+        let {id} = req.query;
+        const response = await deleteData(languageModel,{_id:id});
+        console.log("response",response);
+        
+        if(response) return res.status(404).json(new apiResponse(404,responseMessage.getDataNotFound('Language'),{},{}));
+        return res.status(200).json(new apiResponse(200,responseMessage.deleteDataSuccess('Language'),{},{}));
+
+        
+
 
     }catch(error){
         console.log(error);
