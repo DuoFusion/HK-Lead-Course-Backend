4a43b449a3961a00ee8e9e5558962a21eb2410fb
Palakduofusion <gohelpalak14@gmail.com>
2025-08-23 17:11:13 +0530

    commit

--- src/database/models/workshopRegister.ts
+++ src/database/models/workshopRegister.ts
@@ -7,17 +7,12 @@ const workshopRegisterSchema = new mongoose.Schema({
     phoneNumber: { type: String, required: true },
     city: { type: String },
     profession: { type: String },
-    //  discountType:{type:String,enum:['percentage','fixed'],required:true},
      priority: { type: Number, default: 1 },
-  
     paymentStatus: {type:String, enum: ['paid', 'unpaid'], required: true },
     fees: { type: Number, required: true },
     cupanCodeId: { type: mongoose.Schema.Types.ObjectId, ref: 'cupan-code' },
     paymentMethod: { type: String, required: true },
-    TransactionId: { type: String, required: true },
-
-    // userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
-    // isActive: { type: Boolean, default: true },
+    transactionId: { type: String, required: true },
     isBlocked: { type: Boolean, default: false },
     isDeleted: { type: Boolean, default: false },
 }, { timestamps: true ,versionKey: false })
